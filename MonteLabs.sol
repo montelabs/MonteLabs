9b2125c8b87f065ba22f9284b7a14aa81bcdde74
Fynn <enriquefynn@gmail.com>
2017-11-13 14:01:12 +0100

    init

---
+++ MonteLabs.sol
@@ -0,0 +1,29 @@
+pragma solidity ^0.4.15;
+import "utils.sol";
+
+contract MonteLabs {
+
+  struct Audit {
+    address auditedContract;
+    uint auditLevel;
+  }
+
+  // MonteLabs owners
+  mapping (address => bool) owners;
+  // Maps code keccak256 hasha to Audit
+  mapping (bytes32 => Audit) auditedContracts;
+  event AttachedEvidence(string ipfsHash);
+
+  function MonteLabs(address[] _owners) {
+    require(_owners.length <= 5);
+    for (uint i = 0; i < _owners.length; ++i)
+      owners[_owners[i]] = true;  
+  }
+  
+  function isAddressVerified(address addr) constant returns(uint) {
+    var codeHash = keccak256(GetCode(addr));
+    return auditedContracts[codeHash].auditLevel;
+  }
+  
+  function addAudit();
+}

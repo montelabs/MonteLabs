abb32357a6a8b57d724a954abc1685006f9b357b
Fynn <enriquefynn@gmail.com>
2017-12-09 13:48:57 +0100

    montelabs contracts deployed, scripts

--- contracts/scripts/unifyJson.py
+++ contracts/scripts/unifyJson.py
@@ -1,12 +1,23 @@
 #!/usr/bin/env python
 import json
+import sys
 
 def writeJSON(binary, ABI, filename):
   with open(filename, 'w') as jsonFile:
-    obj = {'abi': ABI, 'bin': binary}
+    obj = {'abi': ABI, 'bin': binary, 'deployed': {}}
     json.dump(obj, jsonFile, indent=2)
 
+def markDeployed(jsonFilename, networkId, address):
+  with open(jsonFilename, 'r') as jsonContract:
+    jsonFile = json.load(jsonContract)
+    jsonFile['deployed'][networkId] = address
+  with open(jsonFilename, 'w') as jsonContract:
+    json.dump(jsonFile, jsonContract, indent=2)
+
 if __name__ == '__main__':
+  if len(sys.argv) > 1:
+    markDeployed(sys.argv[1], sys.argv[2], sys.argv[3])
+    sys.exit(0)
   monteLabsContractPath = '../compiledContracts/MonteLabs'
   monteLabsMSPath = '../compiledContracts/MonteLabsMS'
   with \

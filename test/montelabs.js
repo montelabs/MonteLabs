66a06a2007af2c0981808063c028eea4bcecaf21
Fynn <enriquefynn@gmail.com>
2018-02-18 20:00:48 +0100

    Merge

--- test/montelabs.js
+++ test/montelabs.js
@@ -1,8 +1,11 @@
+const web3 = require('web3');
 const MonteLabs   = artifacts.require('MonteLabs');
 const MonteLabsMS = artifacts.require('MonteLabsMS');
 
 let monteLabsInstance;
 let monteLabsMS;
+
+let l_pk, r_pk, f_pk;
 
 contract('MonteLabs', function(accounts) {
   let montelabsCreator = accounts[0];
@@ -16,11 +19,15 @@ contract('MonteLabs', function(accounts) {
   });
   
   it('Should create MonteLabsMS contract', async () => {
-    let addresses = [l, r, f];
-    monteLabsMS = await MonteLabsMS.new(addresses, monteLabsInstance.address, {from: montelabsCreator});
+    let addresses;
+    console.log('ACC', web3.eth)
+    // console.log(web3.eth.accounts.create());
+    // monteLabsMS = await MonteLabsMS.new(addresses, monteLabsInstance.address, {from: montelabsCreator});
   });
 
-  it('Should add a new Audit', async () => {
-    
-  });
+  // it('Should add a new Audit', async () => {
+  //   let hash = 
+
+  //   monteLabsMS.addAudit
+  // });
 });
